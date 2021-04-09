08febd4279674b1cb48d869263034c39c15a8888
UntitledOutput <79727016+UntitledOutput@users.noreply.github.com>
2021-04-09 10:27:50 -0400

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,16 +1,24 @@
+local min = -10
+local max = 10
+
 local tbl = {
-  math.random(1, 10),
-  math.random(1, 10),
-  math.random(1, 10),
-  math.random(1, 10),
-  math.random(1, 10),
-  math.random(1, 10),
-  math.random(1, 10),
-  math.random(1, 10),
+  math.random(min, max),
+  math.random(min, max),
+  math.random(min, max),
+  math.random(min, max),
+  math.random(min, max),
+  math.random(min, max),
+  math.random(min, max),
+  math.random(min, max),
+  math.random(min, max),
+  math.random(min, max),
+  math.random(min, max),
 }
 
 for i, v in pairs(tbl) do
   if v > 5 then
     print(v.." is over 5!")
+  else
+    print(v.."is not over 5.")
   end
 end
