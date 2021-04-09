908d285f0238ef6e828a2afecc43d36503286abb
UntitledOutput <79727016+UntitledOutput@users.noreply.github.com>
2021-04-09 10:26:09 -0400

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,18 +1,16 @@
 local tbl = {
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  10
+  math.random(1, 10),
+  math.random(1, 10),
+  math.random(1, 10),
+  math.random(1, 10),
+  math.random(1, 10),
+  math.random(1, 10),
+  math.random(1, 10),
+  math.random(1, 10),
 }
 
-for i, v in pairs() do
+for i, v in pairs(tbl) do
   if v > 5 then
-    print("This variable is over 5!")
+    print(v.." is over 5!")
   end
 end
